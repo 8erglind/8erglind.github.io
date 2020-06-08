c5813be5a02b17ef64b3273bac8317deaf8718d0
Berglind Brá Jóhannsdóttir <berglindbrajohannsdottir@Berglinds-MacBook-Pro.local>
2020-06-08 14:47:04 +0200

    svelte working?

---
+++ fan-heat/svelte-app/src/footer.svelte
@@ -0,0 +1,25 @@
+<script></script>
+
+
+
+<div class="footer">
+	
+</div>
+
+
+
+<style>
+	.footer {
+		position: fixed;
+		border-top: 1px solid rgb(70,70,70);
+		height: 5vh;
+		width: 100vw;
+		left: 0vw;
+		bottom: 0vh;
+	}
+
+
+
+
+	
+</style>
